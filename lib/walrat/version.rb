428ca10b9045d00bd8730a85a45b2d9df58e6c38
Greg Hurrell <greg@hurrell.net>
2014-10-19 21:11:37 -0700

    Bump version number to 0.2 in preparation for release
    Will need a new version number so that Walrus can depend on it.

--- lib/walrat/version.rb
+++ lib/walrat/version.rb
@@ -2,6 +2,6 @@
 # Licensed under the terms of the BSD 2-clause license.
 
 module Walrat
-  VERSION   = '0.1.0.99'
+  VERSION   = '0.2'
   COPYRIGHT = 'Copyright 2007-2014 Greg Hurrell'
 end # module Walrat
